e5903cd4092a92b5e706259c8070d48cff2b6627
Godhanded <aezeigwe2003@gmail.com>
2025-10-01 00:52:44 +0200

    Update project title from "PrepWise" to "Prep AI" in layout files

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -16,7 +16,7 @@ const Layout = async ({ children }: { children: ReactNode }) => {
       <nav>
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
-          <h2 className="text-primary-100">PrepWise</h2>
+          <h2 className="text-primary-100">Prep AI</h2>
         </Link>
       </nav>
 
